399c858fd8627f28d8bad5dfce98c7c49631d26b
tmfflavndn <tmfflavndn@gmail.com>
2025-07-17 17:02:38 +0900

    5장 추가

---
+++ 5장_데이터베이스객체.sql
@@ -0,0 +1,17 @@
+/*
+    날짜 : 2025/07/17
+    이름 : 우지희
+    내용 : 5장 데이터베이스 객체
+*/
+
+--워크북 P13
+--실습하기 3-1
+SELECT * FROM DICTIONARY;
+
+SELECT TABLE_NAME FROM USER_TABLES;
+SELECT OWNER, TABLE_NAME FROM ALL_TABLES;
+SELECT * FROM DBA_TABLES;
+
+--실습하기 3-2
+select * from user
+
